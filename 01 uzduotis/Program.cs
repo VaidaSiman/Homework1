09e975fc6d5efffe144df0216b10d711a1bf5b2a
VaidaSiman <vaida.simanauskaite@gmail.com>
2023-03-29 23:32:53 +0300

    Add 02 uzduotis code for variables and outputing to console

---
+++ 01 uzduotis/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_uzduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Labas Vaida");
+            Console.WriteLine();
+
+            int amzius = 30; 
+            Console.WriteLine("Įvestas amžius: " + amzius);
+            Console.WriteLine();
+
+            int skaicius = 25; 
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.Write(skaicius);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(skaicius + " " + skaicius + " " + skaicius + " " + skaicius + " " + skaicius);
+            Console.WriteLine();
+        }
+    }
+}
