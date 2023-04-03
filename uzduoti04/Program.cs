a44c20fb99d97902fa6b2f6d80b237960a9d9c49
VaidaSiman <vaida.simanauskaite@gmail.com>
2023-04-03 04:17:33 +0300

    Add uzduotis04 code

---
+++ uzduoti04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzduoti04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lietuviškos raidės
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string marke = "VW";
+            string modelis = "GOLF";
+            int metai = 1998;
+            int rida = 123456; 
+
+            Console.WriteLine("Automobilio {0} {1} {2} m. rida - {3} km", marke, modelis,  metai, rida);
+
+
+        }
+    }
+}
