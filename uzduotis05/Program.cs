7edb2e03c401b42c3494a99c0ecf10733f3a2ccc
VaidaSiman <vaida.simanauskaite@gmail.com>
2023-04-03 04:57:50 +0300

    Add uzduotis05 code

---
+++ uzduotis05/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzduotis05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lietuviškos raidės
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int skaicius = 5;
+            int numeris = 6;
+            int indeksas = 3;
+            int suma = skaicius + numeris;
+
+            Console.WriteLine("{0} + {1} = {2}",skaicius, numeris,suma);
+          
+            int skirtumas = skaicius - numeris;
+            Console.WriteLine("{0} - {1} = {2}", skaicius, numeris, skirtumas);
+
+            int sandauga = skaicius * numeris;
+            Console.WriteLine("{0} * {1} = {2}", skaicius, numeris, sandauga);
+
+            int dalyba = skaicius / numeris;
+            Console.WriteLine("{0} / {1} = {2}", skaicius, numeris, dalyba);
+            Console.WriteLine();
+
+            Console.WriteLine(skaicius);
+            Console.WriteLine(Math.Pow(skaicius,2));
+            Console.WriteLine(Math.Pow(skaicius, 3));
+            Console.WriteLine();
+            Console.WriteLine(skaicius * numeris * indeksas);
+
+
+
+        }
+    }
+}
