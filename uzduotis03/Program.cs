3e33c87f1292255c29861d56e19fea2f5f81ef4d
VaidaSiman <vaida.simanauskaite@gmail.com>
2023-04-03 03:59:34 +0300

    Add uzduotis03 code for variables and outputing to console

---
+++ uzduotis03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzduotis03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lietuviškos raidės
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string pilnaeilute = "******";
+            string tusciaeilute = "*    *";
+
+            Console.WriteLine(pilnaeilute);
+            Console.WriteLine(tusciaeilute);
+            Console.WriteLine(tusciaeilute);
+            Console.WriteLine(pilnaeilute); 
+                }
+    }
+}
