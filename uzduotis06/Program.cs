d3aa457a0f67d7f6524c91e83b50053f2a54f988
VaidaSiman <vaida.simanauskaite@gmail.com>
2023-04-03 05:57:36 +0300

    Add uzduotis06 code

---
+++ uzduotis06/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uzduotis06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lietuviškos raidės
+            Console.OutputEncoding = Encoding.UTF8;
+
+            int skaicius = 4;
+            int numeris = 0;
+            Console.WriteLine("{0} * {1} = {2}", skaicius, numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine("{0} * {1} = {2}", skaicius, ++numeris, (skaicius * numeris));
+            Console.WriteLine();
+
+            float dalmuo = 12.00f;
+            float atsakymas = skaicius / dalmuo;
+            Console.WriteLine(atsakymas);
+
+        }
+    }
+}
