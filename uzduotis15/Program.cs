0c44f8fd507f7de10a15215b40bc1bf725341807
VaidaSiman <vaida.simanauskaite@gmail.com>
2023-04-14 21:21:31 +0300

    Add uzduotis15 code

---
+++ uzduotis15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace uzduotis15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+        }
+    }
+}
