489c0c1f4af2cee6cd6b4bb0e538e6dc71bd6cd3
VaidaSiman <vaida.simanauskaite@gmail.com>
2023-04-23 23:08:31 +0300

    Add uzduotis28 code

---
+++ uzduotis28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace uzduotis28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            /*
+             * Aprašykite ir iškvieskite metodą, kuris išvestų bet kokį jūsų pasirinktą tekstą.
+             * PROGRAMA PRADĖJO DARBĄ */
+            Tekstas("pROGRAMA PRADĖJO DARBĄ");
+        }
+
+        static private void Tekstas(string betKoksTekstas)
+        {
+            Console.WriteLine($"{betKoksTekstas}");
+        }
+    }
+}
