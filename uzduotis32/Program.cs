d84c7969e991cb6d0467e877c999d87aa8c33fc3
VaidaSiman <vaida.simanauskaite@gmail.com>
2023-04-29 23:48:38 +0300

    Add uzduotis32 code

---
+++ uzduotis32/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace uzduotis32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            /*
+             * Paprašyti įvesti vartotojo vardą.
+             * Sukurti string tipo metodą, kuris gražina frazę pvz „Sveiki, Mantas“.
+             * Išvesti šią frazę į konsolę
+             */
+
+            Console.WriteLine("Įveskite savo vardą: ");
+            string vardas = Console.ReadLine();
+
+            Console.WriteLine(Vartotojas(vardas));
+        }
+
+        private static string Vartotojas(string vardas)
+        {
+            return ($"Sveiki, {vardas}!");
+        }
+    }
+}
